ca1fdd1c2ad6a5032f117e66e8b3fbf7c60b25b7
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2024-10-26 16:34:40 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default App = () => {
+
+  const [buttonStyle, setButtonStyle] = useState(styles.button);
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Hello world!</Text>
+			<StatusBar hidden={true}/>
+			<TouchableOpacity
+				style={buttonStyle}
+				onPressIn={() => setButtonStyle(styles.clickedButton)}
+				onPressOut={() => setButtonStyle(styles.button)}>
+				<Text style={styles.buttonText}>click on me</Text>
+			</TouchableOpacity>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	container: {
+		flex: 1,
+		backgroundColor: '#6D214F',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		fontSize: 45,
+		color: 'white',
+		textDecorationLine: 'underline',
+	},
+	button: {
+		margin: 50,
+		padding: 20,
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: 'white',
+		borderStyle: 'solid'
+	},
+  clickedButton: {
+    margin: 50,
+		padding: 20,
+		borderRadius: 10,
+		borderWidth: 1,
+		borderColor: 'white',
+		borderStyle: 'solid',
+    backgroundColor: 'white'
   },
+	buttonText: {
+		color: 'white',
+		fontSize: 20,
+	},
 });
